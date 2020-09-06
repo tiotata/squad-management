0d6fd201bb39d4cda22113bbab31d23837d925b9
Tarcisio ( Tatá) <taper1513@gmail.com>
2020-09-06 02:58:44 -0300

    more changes

--- src/components/ConfigureSquads.jsx
+++ src/components/ConfigureSquads.jsx
@@ -9,16 +9,22 @@ class AppHeader extends React.Component {
     this.soccerFormationLines = [];
     this.slotKey = 0;
     this.playerListContainers = [];
-    this.state = {
-      playerListContainers: []
+    this.squadConfig = []
+    if (props.squadConfig) {
+      this.squadConfig = props.squadConfig;
     }
+    this.state = {
+      playerListContainers: [],
+      playersSearch: []
+    }
+
 
     //first we read the array containing the formation numbers
     this.formation.map(function (num) {
       let line = [];
       for (let i = 1; i <= num; i++) {
         //and we add as many players we will have horizontally in each line.
-        line.push(<div key={"player" + this.slotKey} className="playerName"><span>+</span></div>);
+        line.push(<div key={"player" + this.slotKey} id={"player" + this.slotKey} onDrop={event => this.drop(event)} onDragOver={event => this.allowDrop(event)} className="playerName"><span>+</span></div>);
         this.slotKey++;
       }
       line.key = "line" + num;
@@ -32,49 +38,106 @@ class AppHeader extends React.Component {
     }
 
     //finally, we add the goal Keeper!
-    this.soccerFormationDiv.push(<div key={this.soccerFormationLines.length} className="formationLine"><div id={this.key} className="playerName"><span>+</span></div></div>)
+    this.soccerFormationDiv.push(<div key={this.soccerFormationLines.length} className="formationLine"><div id={"player" + this.slotKey} onDrop={event => this.drop(event)} onDragOver={event => this.allowDrop(event)} className="playerName"><span draggable="false">+</span></div></div>)
+    this.populateSquadSlots();
+  }
+
+
+  drop(ev) {
+    ev.preventDefault();
+    let target = ev.target;
+    if (target.tagName === "SPAN") {
+      target = target.parentElement;
+    }
+    let player = this.getPlayerFromSearchList(ev.dataTransfer.getData("playerId"));
+    player.initials = this.getPlayerInitials(player);;
+    player.slot = target.id;
+    this.squadConfig.push(player);
+    document.getElementById(target.id.toString()).innerHTML = player.initials;
+    this.playersSearch.splice(this.playersSearch.indexOf(player), 1);
+    this.populateSearchList();
 
   }
 
-  drag(target) {
-    console.log(target);
-
+  getPlayerInitials(player) {
+    let firstNameChar = player.firstname.charAt(0).toUpperCase();;
+    let lastNameChar = player.lastname.charAt(0).toUpperCase();;
+    return firstNameChar + lastNameChar;
   }
 
+  getSquatConfig() {
+    return this.squadConfig;
+  }
+
+  populateSearchList() {
+    this.playerListContainers = [];
+    let players = this.playersSearch;
+    for (let i = 0; i < players.length; i++) {
+      let player = players[i];
+      this.playerListContainers.push(<div className="playerItem" id={player.id} key={player.id} draggable="true" onDragStart={event => this.drag(event)}>
+        <div>
+          <span className="searchLabel">Name: </span>
+          <span>{player.firstname + " " + player.lastname}</span>
+          <span className="searchLabel">Nationality: </span>
+          <span>{player.nationality}</span>
+        </div>
+        <div>
+          <span className="searchLabel">Age: </span>
+          <span>{player.age}</span>
+        </div>
+      </div>);
+    }
+    this.setState({
+      playerListContainers: this.playerListContainers,
+    });
+  }
+
+  populateSquadSlots(){
+    let slot;
+    let player;
+    for ( let i = 0; i < this.squadConfig.length; i++){
+      player = this.squadConfig[i]
+      slot =  document.getElementById(player.slot)
+      slot.innerHTML = player.initials;
+    }
+  }
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData("playerId", ev.target.id);
+  }
+
+  getPlayerFromSearchList(playerId) {
+    return this.playersSearch.find(x => x.id === parseInt(playerId));
+  }
+
+  getPlayerFromSquadList(playerId) {
+    return this.squadConfig.find(x => x.id === parseInt(playerId));
+  }
   searchPlayer(str) {
     if (str.length > 3) {
       let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-      let targetUrl = 'https://www.api-football.com/demo/v2/players/search/ronaldo'
+      let targetUrl = 'https://www.api-football.com/demo/v2/players/search/' + str;
       fetch(proxyUrl + targetUrl)
         .then(res => res.json())
         .then((data) => {
-          this.playerListContainers = [];
-          let players = [];
+          this.playersSearch = [];
           console.log(data);
           for (let i = 0; i < data.api.results; i++) {
-            players.push({
+            if (this.getPlayerFromSearchList(data.api.players[i].player_id) || this.getPlayerFromSquadList(data.api.players[i].player_id))
+              continue;
+            this.playersSearch.push({
               id: data.api.players[i].player_id,
-              firstname: data.api.players[i].fisrtname,
+              firstname: data.api.players[i].firstname,
               lastname: data.api.players[i].lastname,
               nationality: data.api.players[i].nationality,
               age: data.api.players[i].age
             })
-            this.playerListContainers.push(<div className="playerItem" key={data.api.players[i].player_id} draggable="true" onDragStart={event => this.drag(event.target)}>
-              <div>
-                <span className="searchLabel">Name: </span>
-                <span>{data.api.players[i].firstname + " " + data.api.players[i].lastname}</span>
-                <span className="searchLabel">Nationality: </span>
-                <span>{data.api.players[i].nationality}</span>
-              </div>
-              <div>
-                <span className="searchLabel">Age: </span>
-                <span>{data.api.players[i].age}</span>
-              </div>
-            </div>);
           }
-          this.setState({
-            playerListContainers: this.playerListContainers,
-          });
+          this.populateSearchList()
         })
         .catch(console.log)
     }
